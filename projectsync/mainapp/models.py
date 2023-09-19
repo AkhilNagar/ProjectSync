5bf7a7c6af79dc9d6f176fad6b79661067ba28f3
Galahad2819 <akhilnagar19@gmail.com>
2023-09-19 21:45:29 +0530

    Added Auth

--- projectsync/mainapp/models.py
+++ projectsync/mainapp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from  django.contrib.auth.models import User
 
+def __str__(self):
+    #username=self.user.username
+    return self.user.username
 # Create your models here.
